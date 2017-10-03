1d190eea4d809e38840f327668f3db4c3a06fbdb
Mykhailo Rybnikov <mihasic@gmail.com>
2017-10-03 10:39:54 +0200

    remove unused method

--- src/LocalProjections/Extensions.cs
+++ src/LocalProjections/Extensions.cs
@@ -7,7 +7,7 @@ namespace LocalProjections
     using LightningStore;
     using LuceneSearch;
 
-    public static class Extensions
+    internal static class Extensions
     {
         public static async Task HandleException(this Func<Task> action, Action<Exception> onError = null)
         {
@@ -19,36 +19,6 @@ namespace LocalProjections
             {
                 onError?.Invoke(ex);
             }
-        }
-
-        public static CachingRepository<Guid, TDocument> CreateSearchAwareRepository<TDocument>(
-            ObjectRepository<Guid, TDocument> repository,
-            Index index,
-            string idFieldName,
-            Func<TDocument, IEnumerable<KeyValuePair<string, string>>> extractDocument,
-            CancellationToken cancellationToken)
-             where TDocument : new()
-        {
-            return new CachingRepository<Guid, TDocument>(
-                repository,
-                onCommittedDeletes: ds =>
-                {
-                    if (!cancellationToken.IsCancellationRequested)
-                    {
-                        foreach (var d in ds)
-                            index.DeleteByTerm(idFieldName, d.ToString(), commit: false);
-                    }
-                },
-                onCommittedUpserts: dict =>
-                {
-                    foreach (var p in dict)
-                    {
-                        if (cancellationToken.IsCancellationRequested) return;
-                        var doc = extractDocument(p.Value);
-                        index.UpdateByTerm(idFieldName, p.Key.ToString(), doc, commit: false);
-                    }
-                    index.Commit();
-                });
         }
     }
 }
